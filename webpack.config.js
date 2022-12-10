5471c12b6865849f6e86bbb2d042486adff94e8b
Atsu <kamnknon@gmail.com>
2022-12-10 15:26:11 +0900

    Add: create app template

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  // ビルドのエントリーポイント
+  entry: './src/index.ts',
+  // ビルドされたJavaScriptファイルの出力先
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    //.ts(x)?$の正規表現にマッチするファイルを，ts-loaderというWebpackのプラグインを使用してコンパイルする
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      //.css$ という正規表現にマッチするファイルを、style-loader と css-loader という2つのWebpackのプラグインを使用して、CSSを適切な形に変換するように指定．
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  // Webpackが自動的にインポートするモジュールのファイル拡張子
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  }
+};
+
+module.exports = config;
