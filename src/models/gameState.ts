747e40a1509f582ef0821ac64cc9b3c3d1bee7fc
Atsu <kamnknon@gmail.com>
2022-12-12 11:14:37 +0900

    Update:テトリミノの上方向への移動をできないように変更

--- src/models/gameState.ts
+++ src/models/gameState.ts
@@ -168,8 +168,8 @@ export class GameState {
         this._currentTetrimino = this.checkAndMoveLeft();
       } else if (e.key == "ArrowRight") {
         this._currentTetrimino = this.checkAndMoveRight();
-      } else if (e.key == "ArrowUp") {
-        this._currentTetrimino = this.checkAndMoveUp();
+        //} else if (e.key == "ArrowUp") {
+        //this._currentTetrimino = this.checkAndMoveUp();
       } else if (e.key == "ArrowDown") {
         this._currentTetrimino = this.checkAndMoveDown();
       } else if (e.key == " ") {
@@ -201,11 +201,11 @@ export class GameState {
     return this._currentTetrimino;
   }
 
-  private checkAndMoveUp(): Tetrimino {
-    let newTetrimino = this._currentTetrimino.moveUp();
-    if (this.checkMove(newTetrimino)) return newTetrimino;
-    return this._currentTetrimino;
-  }
+  //private checkAndMoveUp() {
+    // とりあえず今は何もしない
+    // 今後，新しい処理を追加する可能性あり
+  //}
+
   private checkAndMoveDown(): Tetrimino {
     let newTetrimino = this._currentTetrimino.moveDown();
     if (this.checkMove(newTetrimino)) return newTetrimino;
