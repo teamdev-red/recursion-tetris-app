f9262f096be361cce9c8093067b6ec12d521c3eb
Youichi-Higa <yo1_1023@yahoo.co.jp>
2022-12-13 18:16:19 +0900

    Add: 次のテトリミノ表示エリアのdiv要素を作成する関数

---
+++ src/components/nextArea.ts
@@ -0,0 +1,16 @@
+/**
+ * 次のテトリミノ表示エリアのdiv要素を作成する関数
+ * @return {HTMLDivElement} 次のテトリミノ表示エリアのdiv要素
+ */
+export const createNextArea = (): HTMLDivElement => {
+  let container = document.createElement('div');
+  container.classList.add('col-4');
+
+  container.innerHTML = `
+    <div class="next-area bg-dark p-2">
+      <p class="h5 text-center text-white">NEXT</p>
+    </div>  
+  `;
+
+  return container;
+};
