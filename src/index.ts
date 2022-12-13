710526829e238c6bbed54f21620cdb5123e5b6b5
Atsu <kamnknon@gmail.com>
2022-12-13 13:35:37 +0900

    Add: Bootstrapを使用できるようライブラリをinstall

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
 import "./styles.css";
 
 import { GameState } from "./models/gameState";
 
 let app = document.querySelector("#app");
-let can = document.createElement("canvas");
-app.appendChild(can);
 
-const gameState = new GameState(can);
-gameState.gameStart();
+//let can = document.createElement("canvas");
+//app.appendChild(can);
+
+//const gameState = new GameState(can);
+//gameState.gameStart();
