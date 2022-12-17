461724f63c41e940ecacac1d549d67dfe8667861
Youichi-Higa <yo1_1023@yahoo.co.jp>
2022-12-17 17:34:04 +0900

    fix: JSDoc修正

--- src/components/holdArea.ts
+++ src/components/holdArea.ts
@@ -1,6 +1,6 @@
 /**
- * スコア表示エリアのdiv要素を作成する関数
- * @return {HTMLDivElement} スコア表示エリアのdiv要素
+ * ホールドしたテトリミノ表示エリアのdiv要素を作成する関数
+ * @return {HTMLDivElement} ホールドしたテトリミノ表示エリアのdiv要素
  */
 export const createHoldArea = (): HTMLDivElement => {
   let container = document.createElement('div');
