83a18db24dbf0a02a59042e4fb758b0fdcf049c3
Youichi-Higa <yo1_1023@yahoo.co.jp>
2022-12-18 18:00:53 +0900

    fix: createBootstrapSelect-function
    id設定、value変更

--- src/components/bootstrapSelect.ts
+++ src/components/bootstrapSelect.ts
@@ -6,14 +6,18 @@ export const createBootstrapSelect = (): HTMLDivElement => {
   let container = document.createElement('div');
   container.classList.add('mx-auto');
   container.style.width = '200px';
-  
+
   container.innerHTML = `
     <p>DIFFICULT LEVEL</p>
-    <select class="custom-select custom-select-lg" style="background-color: aliceblue;">
-      <option value="500">EASY</option>
-      <option value="400" selected>NORMAL</option>
-      <option value="300">HARD</option>
-    </select>  
+      <select
+        id="difficultLevel"
+        class="custom-select custom-select-lg shadow"
+        style="background-color: aliceblue"
+      >
+        <option value="easy">EASY</option>
+        <option value="normal" selected>NORMAL</option>
+        <option value="hard">HARD</option>
+      </select>
   `;
 
   return container;
