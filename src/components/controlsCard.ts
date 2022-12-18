9e78982e3b3f502ee6f3544982797367d1853c2f
Youichi-Higa <yo1_1023@yahoo.co.jp>
2022-12-18 11:28:09 +0900

    Add: 操作方法を表示

--- src/components/controlsCard.ts
+++ src/components/controlsCard.ts
@@ -4,25 +4,19 @@
  */
 export const createControlsCard = (): HTMLDivElement => {
   let container = document.createElement('div');
-  container.classList.add(
-    'controls-card',
-    'd-flex',
-    'justify-content-center',
-    'shadow-lg',
-    'py-3',
-    'bg-light',
-    'rounded'
-  );
+  container.classList.add('d-flex', 'justify-content-sm-end', 'mt-4');
 
   container.innerHTML = `
-    <div>
-      <p class="font-weight-bold">右矢印キー：右に移動</p>
-      <p class="font-weight-bold">左矢印キー：左に移動</p>
-      <p class="font-weight-bold">上矢印キー：一番下まで落下</p>
-      <p class="font-weight-bold">下矢印キー：下に移動</p>
-      <p class="font-weight-bold">スペースキー：回転</p>
-      <p class="font-weight-bold">シフトキー：ホールド</p>
-      <p class="font-weight-bold">Pキー：ポーズ，再開</p>
+    <div class="controls-card d-flex justify-content-center shadow-lg py-3 bg-light rounded">
+      <div>
+        <p class="font-weight-bold">右矢印キー：右に移動</p>
+        <p class="font-weight-bold">左矢印キー：左に移動</p>
+        <p class="font-weight-bold">上矢印キー：一番下まで落下</p>
+        <p class="font-weight-bold">下矢印キー：下に移動</p>
+        <p class="font-weight-bold">スペースキー：回転</p>
+        <p class="font-weight-bold">シフトキー：ホールド</p>
+        <p class="font-weight-bold">Pキー：ポーズ，再開</p>
+      </div>
     </div>
   `;
 
